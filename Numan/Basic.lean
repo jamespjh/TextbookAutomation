0dff6c97a2642d71116aa0e39a2a9716e754cc56
James Hetherington <j.hetherington@ucl.ac.uk>
2025-11-26 17:29:24 +0000

    State the simple definition of a limit

---
+++ Numan/Basic.lean
@@ -0,0 +1,24 @@
+-- Working through Numerical Analysis by Burden and Faires
+-- Chapter 1: Mathematical Preliminaries
+
+-- 1.1 Review of calculus
+
+-- Definition 1.1
+
+-- definition of a limit of a function
+-- state the definition from the book and then find the corresponding
+-- definition in Mathlib and prove them equivalent
+
+-- Basically, take the sophisticated theory of filters
+-- (see https://leanprover-community.github.io/mathematics_in_lean/C11_Topology.html#filters)
+-- and prove that it matches the epsilon-delta definition
+
+import Mathlib.Algebra.Order.Group.Abs
+import Mathlib.Data.Real.Basic
+
+#eval |(-4:ℤ )|
+
+namespace Burden
+def Tendsto (f : ℝ → ℝ) (x₀ L : ℝ) : Prop :=
+  ∀ ε > 0, ∃ δ > 0, ∀ x, (0 < |x - x₀| ∧ |x - x₀| < δ) → |f x - L| < ε
+end Burden

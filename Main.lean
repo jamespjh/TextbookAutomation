85dbce557636eddbaecc192821ea68f5c31c7efa
James Hetherington <j.hetherington@ucl.ac.uk>
2025-11-26 17:56:05 +0000

    Define tendsto for reals using metric space instance and filters

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import Numan
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
--- Numan/Basic.lean
+++ Numan/Basic.lean
@@ -16,9 +16,15 @@
 import Mathlib.Algebra.Order.Group.Abs
 import Mathlib.Data.Real.Basic
 
+import Mathlib.Order.Filter.Tendsto
+import Mathlib.Topology.Defs.Filter
+import Mathlib.Topology.MetricSpace.Basic
+
 #eval |(-4:ℤ )|
 
 namespace Burden
-def Tendsto (f : ℝ → ℝ) (x₀ L : ℝ) : Prop :=
-  ∀ ε > 0, ∃ δ > 0, ∀ x, (0 < |x - x₀| ∧ |x - x₀| < δ) → |f x - L| < ε
+  def Tendsto (f : ℝ → ℝ) (x₀ L : ℝ) : Prop :=
+    ∀ ε > 0, ∃ δ > 0, ∀ x, (0 < |x - x₀| ∧ |x - x₀| < δ) → |f x - L| < ε
 end Burden
+
+def RTendsto (f : ℝ → ℝ) (x₀ L : ℝ) : Prop := Filter.Tendsto f (nhds x₀) (nhds L)

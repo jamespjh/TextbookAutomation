53d803d3a50a1a81af53a3799bc90ccb55a9e057
James Hetherington <j.hetherington@ucl.ac.uk>
2025-12-10 19:35:49 +0000

    Working half of proof

--- Numan/Basic.lean
+++ Numan/Basic.lean
@@ -20,8 +20,6 @@ import Mathlib.Order.Filter.Tendsto
 import Mathlib.Topology.Defs.Filter
 import Mathlib.Topology.MetricSpace.Basic
 
-#eval |(-4:ℤ )|
-
 namespace Burden
   def Tendsto (f : ℝ → ℝ) (x₀ L : ℝ) : Prop :=
     ∀ ε > 0, ∃ δ > 0, ∀ x, (0 < |x - x₀| ∧ |x - x₀| < δ) → |f x - L| < ε
@@ -29,8 +27,46 @@ end Burden
 
 open Topology Filter
 
-def RTendsto (f : ℝ → ℝ) (x₀ L : ℝ) : Prop := Filter.Tendsto f (𝓝 x₀) (𝓝 L)
+def RTendsto (f : ℝ → ℝ) (x₀ L : ℝ) : Prop := Filter.Tendsto f (𝓝[≠] x₀) (𝓝 L)
 
 theorem RTendsto_iff_BurdenTendsto (f : ℝ → ℝ) (x₀ L : ℝ) :
     Burden.Tendsto f x₀ L ↔ RTendsto f x₀ L := by
-  sorry
+  apply Iff.intro
+  · unfold RTendsto
+    unfold Tendsto
+    intro h
+    unfold Burden.Tendsto at h
+    simp [sub_ne_zero] at h
+
+    -- Unpack the filter definition of tendsto --
+    apply Filter.le_def.mpr
+    simp
+    unfold Set.preimage
+    intro cn cnh
+
+    -- Use the definition of neighborhood in the metric space of reals --
+    apply Metric.mem_nhds_iff.mp at cnh
+    apply Metric.mem_nhdsWithin_iff.mpr
+    simp only [Metric.ball, Real.dist_eq] at *
+    rcases cnh with ⟨ε₂, ε₂pos, hε₂⟩
+
+    -- Clean up set member expressions in the goal and hypothesis --
+    change (∃ δ > 0, ∀ x, x∈ ({y | |y - x₀| < δ}  ∩ {x₀}ᶜ ) -> f x ∈ cn)
+    simp only [Set.mem_inter_iff, Set.mem_setOf_eq, Set.mem_compl_iff, Set.mem_singleton_iff]
+    have hε₂' : ∀ y , |y - L| < ε₂ → y ∈ cn :=
+      Set.mem_setOf_eq.mpr hε₂
+
+    -- Invoke the epsilon-delta definition --
+    have k := h ε₂ ε₂pos
+    rcases k with ⟨δ, δpos, hδ⟩
+    use δ
+    constructor
+    exact δpos
+
+    -- Finish the proof --
+    intro x hx
+    rcases hx with ⟨hx₁, hx₂⟩
+
+    exact hε₂' (f x) (hδ x hx₂ hx₁)
+
+  . sorry

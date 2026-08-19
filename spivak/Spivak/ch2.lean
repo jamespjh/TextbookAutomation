0f74b8479b4f11c9291d132d6decabd7cb227987
James Hetherington <j.hetherington@ucl.ac.uk>
2026-08-20 00:12:35 +0100

    More spivak

--- spivak/Spivak/ch2.lean
+++ spivak/Spivak/ch2.lean
@@ -7,8 +7,33 @@ import Mathlib.Tactic
 
 def myprime (n : ℕ) : Prop := n > 1 ∧ ∀ m : ℕ, m ∣ n → m = 1 ∨ m = n
 
-theorem composite_has_two_factors (n : ℕ) (h : ¬ myprime n) : ∃ a b : ℕ, 1 < a ∧ 1 < b ∧ a * b = n := by
-  sorry
+theorem eq_of_div_eq_one' (m n : ℕ) (hdvd : m ∣ n) (h : n / m = 1) : n = m := by
+  have := Nat.mul_div_cancel' hdvd
+  rw [h, mul_one] at this
+  exact this.symm
+
+theorem composite_has_two_factors (n : ℕ) (h : ¬ myprime n) (hn : n > 1) : ∃ a b : ℕ, 1 < a ∧ 1 < b ∧ a * b = n := by
+  unfold myprime at h
+  have := not_and.mp h
+  have := this (hn)
+  have := not_forall.mp this
+  rcases this with ⟨ m, hm ⟩
+  have := Classical.not_imp.mp hm
+  rcases this with ⟨ h1, h2 ⟩
+  have := not_or.mp h2
+  rcases this with ⟨ h3, h4 ⟩
+  use m, n / m
+  refine ⟨?_, ?_, ?_⟩
+  have : m ≠ 0 := by
+    exact ne_zero_of_dvd_ne_zero (by positivity) h1
+  · have := and_imp.mp ((Nat.two_le_iff m).mpr) (by positivity) h3
+    linarith
+  · have n1 := mt (eq_of_div_eq_one' m n h1) (mt Eq.symm h4)
+    have : n / m ≠ 0 := by
+      sorry
+    have := and_imp.mp ((Nat.two_le_iff (n / m)).mpr) (by positivity) n1
+    linarith
+  · apply Nat.mul_div_cancel' h1
 
 theorem factor_is_less (n a b: ℕ) (h : a * b = n) (h2 : 1 < a) (h3 : 1 < b) :
   a < n := by
@@ -27,7 +52,15 @@ theorem lem_prime_has_prime_factorisation (p : ℕ) (hp : myprime p) : has_prime
     assumption
   · simp
 
-theorem all_has_prime_factorisation (n : ℕ) : has_prime_factorisation n := by
+theorem one_has_prime_factorisation : has_prime_factorisation 1 := by
+  use []
+  constructor
+  · intros p h
+    apply List.forall_mem_nil
+    assumption
+  · simp
+
+theorem all_has_prime_factorisation (n : ℕ) (hn : n > 1) : has_prime_factorisation n := by
   -- cases, either n is prime, in which case the list is just [n],
   -- or n is composite, in which case we can factor it into two smaller numbers,
   -- and then recursively factor those.
@@ -35,9 +68,17 @@ theorem all_has_prime_factorisation (n : ℕ) : has_prime_factorisation n := by
   by_cases prime : myprime l
   · apply lem_prime_has_prime_factorisation l prime
   -- l is composite, so it has a factorisation into two smaller numbers
-  have comp := composite_has_two_factors l prime
+  have comp := composite_has_two_factors l prime (hn)
   rcases comp with ⟨ a, b, ha1, hb1, h2 ⟩
-  have hpfa := ih a (factor_is_less l a b h2 ha1 hb1)
-  rw [mul_comm] at h2
-  have hpfb := ih b (factor_is_less l b a h2 hb1 ha1)
   -- apply the induction hypothesis to a and b
+  have hpfa := ih a (factor_is_less l a b h2 ha1 hb1) ha1
+  rw [mul_comm] at h2
+  have hpfb := ih b (factor_is_less l b a h2 hb1 ha1) hb1
+  -- combine the two lists of prime factors
+  unfold has_prime_factorisation at hpfa hpfb
+  rcases hpfa with ⟨ la, hla, hla2⟩
+  rcases hpfb with ⟨ lb, hlb, hlb2⟩
+  use (la ++ lb)
+  constructor
+  · sorry
+  · sorry

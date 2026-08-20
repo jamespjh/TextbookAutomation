8bdd06d55001b13f119fedf1ed3cd81771ae4c88
James Hetherington <j.hetherington@ucl.ac.uk>
2026-08-20 19:03:32 +0100

    Spivak c2 example

--- spivak/Spivak/ch2.lean
+++ spivak/Spivak/ch2.lean
@@ -12,7 +12,8 @@ theorem eq_of_div_eq_one' (m n : ℕ) (hdvd : m ∣ n) (h : n / m = 1) : n = m :
   rw [h, mul_one] at this
   exact this.symm
 
-theorem composite_has_two_factors (n : ℕ) (h : ¬ myprime n) (hn : n > 1) : ∃ a b : ℕ, 1 < a ∧ 1 < b ∧ a * b = n := by
+theorem composite_has_two_factors (n : ℕ) (h : ¬ myprime n) (hn : n > 1) :
+  ∃ a b : ℕ, 1 < a ∧ 1 < b ∧ a * b = n := by
   unfold myprime at h
   have := not_and.mp h
   have := this (hn)
@@ -22,22 +23,27 @@ theorem composite_has_two_factors (n : ℕ) (h : ¬ myprime n) (hn : n > 1) : �
   rcases this with ⟨ h1, h2 ⟩
   have := not_or.mp h2
   rcases this with ⟨ h3, h4 ⟩
-  use m, n / m
-  refine ⟨?_, ?_, ?_⟩
-  have : m ≠ 0 := by
+  have ⟨p, hp ⟩ := h1
+  use m, p
+  have hn0 : m ≠ 0 := by
     exact ne_zero_of_dvd_ne_zero (by positivity) h1
+  refine ⟨?_, ?_, ?_⟩
   · have := and_imp.mp ((Nat.two_le_iff m).mpr) (by positivity) h3
     linarith
-  · have n1 := mt (eq_of_div_eq_one' m n h1) (mt Eq.symm h4)
-    have : n / m ≠ 0 := by
-      sorry
-    have := and_imp.mp ((Nat.two_le_iff (n / m)).mpr) (by positivity) n1
+  · have : p ≠ 0 := by
+      by_contra neg
+      rw [neg] at hp
+      simp at hp
+      have := ne_of_gt (lt_trans zero_lt_one hn)
+      contradiction
+    have : p ≠ 1 := by
+      by_contra neg
+      rw [neg] at hp
+      simp at hp
+      exact h4 hp.symm
+    have := and_imp.mp ((Nat.two_le_iff (p)).mpr) (by positivity) this
     linarith
-  · apply Nat.mul_div_cancel' h1
-
-theorem factor_is_less (n a b: ℕ) (h : a * b = n) (h2 : 1 < a) (h3 : 1 < b) :
-  a < n := by
-  sorry
+  · exact hp.symm
 
 def has_prime_factorisation (n : ℕ) : Prop :=
   -- exists a list of primes such that the product is n
@@ -64,21 +70,48 @@ theorem all_has_prime_factorisation (n : ℕ) (hn : n > 1) : has_prime_factorisa
   -- cases, either n is prime, in which case the list is just [n],
   -- or n is composite, in which case we can factor it into two smaller numbers,
   -- and then recursively factor those.
-  induction' n using Nat.strong_induction_on with l ih
-  by_cases prime : myprime l
-  · apply lem_prime_has_prime_factorisation l prime
-  -- l is composite, so it has a factorisation into two smaller numbers
-  have comp := composite_has_two_factors l prime (hn)
-  rcases comp with ⟨ a, b, ha1, hb1, h2 ⟩
-  -- apply the induction hypothesis to a and b
-  have hpfa := ih a (factor_is_less l a b h2 ha1 hb1) ha1
-  rw [mul_comm] at h2
-  have hpfb := ih b (factor_is_less l b a h2 hb1 ha1) hb1
-  -- combine the two lists of prime factors
-  unfold has_prime_factorisation at hpfa hpfb
-  rcases hpfa with ⟨ la, hla, hla2⟩
-  rcases hpfb with ⟨ lb, hlb, hlb2⟩
-  use (la ++ lb)
-  constructor
-  · sorry
-  · sorry
+  induction n using Nat.strong_induction_on with
+  | h l ih =>
+      by_cases prime : myprime l
+      · apply lem_prime_has_prime_factorisation l prime
+      -- l is composite, so it has a factorisation into two smaller numbers
+      have comp := composite_has_two_factors l prime (hn)
+      rcases comp with ⟨ a, b, ha1, hb1, h2 ⟩
+      have ln0 := ((Nat.two_le_iff l).mp hn).left
+      have := dvd_of_mul_left_eq a h2
+      have ble := Nat.le_of_dvd (lt_trans zero_lt_one hn) this
+      have := dvd_of_mul_right_eq b h2
+      have ale := Nat.le_of_dvd (lt_trans zero_lt_one hn) this
+      have f1 : a * b = l * 1 := by rw [h2, mul_one]
+      have anl : a ≠ l := by
+        by_contra neg
+        rw [neg] at f1
+        have := Nat.mul_left_cancel (by positivity) f1
+        exact ne_of_gt hb1 this
+      have bnl : b ≠ l := by
+        by_contra neg
+        rw [neg, mul_comm] at f1
+        have := Nat.mul_left_cancel (by positivity) f1
+        exact ne_of_gt ha1 this
+      have al : a < l := lt_of_le_of_ne ale anl
+      have bl : b < l := lt_of_le_of_ne ble bnl
+      -- apply the induction hypothesis to a and b
+      have hpfa := ih a al ha1
+      rw [mul_comm] at h2
+      have hpfb := ih b bl hb1
+      -- combine the two lists of prime factors
+      unfold has_prime_factorisation at hpfa hpfb
+      rcases hpfa with ⟨ la, hla, hla2⟩
+      rcases hpfb with ⟨ lb, hlb, hlb2⟩
+      use (la ++ lb)
+      constructor
+      · intro p hp
+        have := List.mem_append.mp hp
+        cases this
+        · apply hla; assumption
+        · apply hlb; assumption
+      · rw [List.prod_append, hla2, hlb2, mul_comm]
+        assumption
+
+-- Note we've used a few fairly strong facts that it would be nice to prove independently:
+-- List.prod_append, List.mem_append, Nat.le_of_dvd
